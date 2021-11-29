e4b15e870ebaa1f1fbb3fa6800d1e12492656452
shiyuc <shiyuc@students.cs.ubc.ca>
2021-11-29 12:10:30 -0800

    add filter item functionality

--- controllers/items.js
+++ controllers/items.js
@@ -12,7 +12,49 @@ const getAllItems = async (req, res) => {
 }
 
 const getFilteredItems = async (req, res) => {
-    const filteredItems = Item.find(req.query)
+    // a better way to do this is to first
+    // find the valid part of res.query
+    // instead of using req.query directly
+    let { name, distance, condition, sort, selects, limit, page, numericFilters } = req.query
+    queryObj = {}
+    if (name) {
+        queryObj.name = { $regex: name, $options: 'i' }
+    }
+    if (distance) {
+        queryObj.distance = distance
+    }
+    if (condition) {
+        queryObj.condition = condition
+    }
+    console.log(queryObj)
+    const queryDB = Item.find(queryObj)
+    if (sort) {
+        queryDB.sort(sort.replace(/,/g, " "))
+    }
+    if (selects) {
+        console.log(selects.replace(/,/g, " "))
+        queryDB.select(selects.replace(/,/g, " "))
+        // queryDB.select("name age distance")
+    }
+    numericComparisonDict = {
+        ">": "$gt",
+        ">=": "$gte",
+        "=": "$eq",
+        "<": "$lt",
+        "<=": "$lte"
+    }
+    if (numericFilters) {
+        numericFilterList = numericFilters.split(",")
+        numericFilterList.foreach(filter => {
+            filter.replace(/\b(>|>=|=|<|<=)\b/, (match) => `${-numericComparisonDict[match]}`)
+        })
+
+    }
+    page = page || 1
+    limit = limit || 3
+    skip = (page - 1) * limit
+    queryDB.limit(limit).skip(skip)
+    const filteredItems = await queryDB
     res.status(200).json({
         items: filteredItems,
         nbHits: filteredItems.length
